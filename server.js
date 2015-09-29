fb8459ea8841306aa9b9d2db10a98489ba92bbd4
garret.meier <garret.meier@bunchball.com>
2015-09-29 10:08:32 -0500

    changing some minutia

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var app = require("./server/index.js");
 app.set("port", process.env.PORT || 3000);
 
 var server = app.listen(app.get("port"),function(){
-	console.log("Pitchly Stripe Server listening on port " + server.address().port);
+	console.log("Server listening on port " + server.address().port);
 });

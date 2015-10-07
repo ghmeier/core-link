acb48b39cabe2fb0f6b293e755b7c16d82b8df24
garret.meier <garret.meier@bunchball.com>
2015-10-07 14:58:32 -0500

    removed some errors

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
 var secrets = require("./config/secrets");
 
 module.exports = function(app,passport){
-	app.get("/",function(req,res){
+	app.get("/ping",function(req,res){
 
 		res.send("pong");
 	});
